f1bcac1f5d573cbff6ee25ea2a8419f265262327
linuk <IT21274398@my.sliit.lk>
2023-05-04 23:05:03 +0530

    linuka-frontend finished

---
+++ app/src/main/java/com/example/madproject/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.madproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+
+
+    fun goToSecondActivity(view: View) {
+        val intent = Intent(this, itemCategory::class.java)
+        startActivity(intent)
+        }
+    fun goThirdActivity(view: View) {
+        val intent = Intent(this, itemCategory2::class.java)
+        startActivity(intent)
+    }fun goTofourthActivity(view: View) {
+        val intent = Intent(this, itemCategory3::class.java)
+        startActivity(intent)
+    }
+    fun goTofifthActivity(view: View) {
+        val intent = Intent(this, itemCategory4::class.java)
+        startActivity(intent)
+    }
+    fun goTosixthActivity(view: View) {
+        val intent = Intent(this, itemCategory5::class.java)
+        startActivity(intent)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/madproject/itemCategory5.kt
@@ -0,0 +1,12 @@
+package com.example.madproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class itemCategory5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_category5)
+
+    }
+}

8878206513e453f32a2c72afc93fdc4122ecf400
linuk <IT21274398@my.sliit.lk>
2023-05-05 11:14:22 +0530

    linuka-frontend finished

--- app/src/main/java/com/example/madproject/AdminPanel.kt
+++ app/src/main/java/com/example/madproject/AdminPanel.kt
@@ -2,8 +2,13 @@ package com.example.madproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class AdminPanel : AppCompatActivity() {
+
+    private lateinit var itemName: EditText
+    private lateinit var itemPrice: EditText
+    private lateinit var itemimage: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_panel)

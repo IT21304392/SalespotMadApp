c4fe7ad38737fea3632eab85308224854246e8dc
linuk <IT21274398@my.sliit.lk>
2023-05-05 13:54:11 +0530

    linuka-frontend finished

---
+++ app/src/main/java/com/example/madproject/ITEMmodel.kt
@@ -0,0 +1,8 @@
+package com.example.madproject
+
+class ITEMmodel(itemID: Any?, itName: String, itPrice: String, itimage: String) {
+    var itemId: String? = null
+    var itemname :String? = null
+    var itemprice :String? = null
+    var itemimage :String? = null
+}
